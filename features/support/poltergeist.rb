8ec0754c3cc12b503dbd4cd2d848a8a4efd6ac76
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-02-25 18:28:04 -0500

    Revert "Make poltergeist able to show debug web inspector"
    This reverts commit 3493321248df4d0d4827f61a6cdc2ecebf893daf.
    
    Trying to see if this fixes our erroring-out Travis CI situation.

--- features/support/poltergeist.rb
+++ features/support/poltergeist.rb
@@ -1,7 +1,2 @@
 require 'capybara/poltergeist'
-
-Capybara.register_driver :poltergeist_debug do |app|
-  Capybara::Poltergeist::Driver.new(app, :inspector => true)
-end
-
-Capybara.javascript_driver = :poltergeist_debug
+Capybara.javascript_driver = :poltergeist
